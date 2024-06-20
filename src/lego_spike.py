cbabbb58fa4f2589ed7814be56cb896b30b68a71
Vin1X <vincent.walura@hotmail.com>
2024-06-20 12:02:54 +0200

    update PID, fixup

--- src/lego_spike.py
+++ src/lego_spike.py
@@ -14,19 +14,23 @@ class Lego_Spike:
 
         # Left motor is inverted
         self.motor_pair = LegoMotorPair("A", "B") # A = Left, B = Right
-        self.max_speed = 5
+        self.max_speed = 30
         self.max_absolute_difference = 100
-        self.pid = PID(-.1, 0, 0, setpoint=0)
+        self.pid = PID(.5, .1, 0, setpoint=0)
 
     def follow_line(self):
         diff = self.get_diff_double()
         diff = self.compute_pid(diff)
         left_speed, right_speed = self.calculate_steering(diff)
         left_speed *= -1
-        time.sleep(.5)
+        #:time.sleep(.5)
         self.motor_pair.start(speedl=left_speed, speedr=right_speed)
 
     def calculate_steering(self, diff: float) -> tuple:
+        #print(diff)
+        diff = min(100, diff)
+        diff = max(-100, diff)
+        #print("\t\t", diff, "\n")
         reduced_speed = (self.max_speed * 2) / self.max_absolute_difference * 2 * abs(diff)
 
         if diff > 0:
@@ -35,7 +39,7 @@ class Lego_Spike:
         else:
             left_speed = self.max_speed
             right_speed = self.max_speed - reduced_speed
-        print("Reducedspeed:", reduced_speed, "Left:", left_speed, "Right:", right_speed)
+        #print("Reducedspeed:", reduced_speed, "Left:", left_speed, "Right:", right_speed)
         return left_speed, right_speed
 
     def get_diff_single(self, color_sensor) -> float:
@@ -43,13 +47,13 @@ class Lego_Spike:
         return diff
 
     # Positive value = deviation to right
-    # Negative value = deviation to left
+    # Negative valu = deviation to left
     def get_diff_double(self) -> float:
         l_refl = self.color_sensor_l.reflected_light()
         r_refl = self.color_sensor_r.reflected_light()
         return r_refl - l_refl
 
     def compute_pid(self, diff: float) -> float:
-        pid = self.pid(diff)
-        # print("diff:", diff, "pid:", pid)
-        return pid
+        pid_diff = -self.pid(diff)
+        print("diff:", diff, "pid:", pid_diff)
+        return pid_diff
